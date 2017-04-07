e2e4b050acc27043554ad69d8d3d0b7ed9f620df
ankit <ankit.velani@gmail.com>
2017-04-07 15:07:11 +0530

    First Version Commit

--- image classification/index.php
+++ image classification/index.php
@@ -54,20 +54,7 @@ $imageName="";
         <span class="navbar-toggler-icon"></span>
       </button>
       <a class="navbar-brand" href="#">ImageClassification</a>
-      <div class="collapse navbar-collapse" id="navbarCollapse">
-        <ul class="navbar-nav">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#">Disabled</a>
-          </li>
-        </ul>  
-
-      </div>
+       
     </nav>
 
     <!-- Begin page content -->
